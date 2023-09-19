5412aa75b81952e38fcfdaa01442feec0dbf13b7
Fardin7864 <fardinhasan1888@gmail.com>
2023-09-19 20:32:28 +0600

    fetch data from rest countrys api and display on ui and created a file structure

---
+++ src/components/AllCountrys/AllCountrys.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import Country from "./Country";
+
+const AllCountrys = () => {
+    const countrys = useLoaderData();
+    return (
+        <>
+            <div className="grid md:grid-cols-4 grid-cols-1 gap-4">
+            {
+                countrys.map((country,idx) => 
+                    <Country
+                    key={idx}
+                    country={country}
+                    ></Country>)
+            }
+            </div>
+        </>
+    );
+};
+
+export default AllCountrys;
--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,22 @@ import
   createBrowserRouter,
   RouterProvider,
  } from 'react-router-dom'
+import AllCountrys from './components/AllCountrys/AllCountrys.jsx'
+import Users from './components/Users/Users.jsx'
 
  const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>
+    element: <App></App>,
+    children: [
+      {
+        path: '/all',
+        loader: () => fetch('https://restcountries.com/v3.1/all'),
+        element: <AllCountrys></AllCountrys>
+      },
+     
+    ]
   },
-  {
-    path: '/about',
-    element: 
-  }
  ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
